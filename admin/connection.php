7be0a2b4d04ee8006109900ac99aa5f1981a9a2b
Diluka Wijesinghe <92380086+DilukaW@users.noreply.github.com>
2022-06-14 10:06:50 +0530

    Finalized

--- admin/connection.php
+++ admin/connection.php
@@ -7,10 +7,10 @@
     //Creating the connection between the database and the website
     //Getting the database credientials
     define('SITEURL', 'http://localhost/vsafe-web/admin/');
-    define('LOCALHOST', '198.38.88.80:3306');   //Database located server
-    define('DB_USERNAME', 'bimbiave_orisoft');           //username to enter db
-    define('DB_PASSWORD', 'KG_hMzM;v.si9(8o');               //pwrd to enter db
-    define('DB_NAME', 'bimbiave_vsafe');           //database name what we use to store data
+    define('LOCALHOST', '*****');   //Database located server
+    define('DB_USERNAME', '******');           //username to enter db
+    define('DB_PASSWORD', '******');               //pwrd to enter db
+    define('DB_NAME', '********');           //database name what we use to store data
     
     $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD, DB_NAME);// or die(myqli_error());
 
